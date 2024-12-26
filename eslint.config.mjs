f0e73d73be29768a6976ac474348ada9e625b94a
Troy Alford <TroyAlford@users.noreply.github.com>
2024-12-25 20:07:18 -0800

    working linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,18 @@
-import config from 'basis/libraries/eslint-plugin/dist/index.js'
+import basis from 'basis/libraries/eslint-plugin/dist/index.js'
 
-export default config
+export default [
+  ...basis,
+  {
+    files: ['*.mjs'],
+    rules: {
+      '@import/extensions': 'off',
+      '@import/no-default-export': 'off',
+    },
+  },
+  {
+    files: ['**/vite.config.*'],
+    rules: {
+      '@import/no-default-export': 'off',
+    },
+  },
+]
